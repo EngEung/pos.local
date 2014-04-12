5ad22a87488e2d6b65b19796b3ca0783097d1138
Eng Eung <engeung@gmail.com>
2014-04-12 07:06:37 +0700

    customer

--- protected/modules/setup/views/customers/_form.php
+++ protected/modules/setup/views/customers/_form.php
@@ -15,17 +15,17 @@
    		'htmlOptions' => array('class' => 'well-small',), // for inset effect
    ));
 ?>		
-	<div style=" width: 400px; margin: 0; padding:0; float: left; margin-left:-50px;">
+	<div style=" width: 345px; margin: 0; padding:0; float: left;">
 		<?php echo $form->hiddenField($model, 'id'); ?>
 		<?php echo $form->textFieldRow($model, 'firstName', array('placeholder' => '')); ?>
 		<?php echo $form->textFieldRow($model, 'lastName', array('placeholder' => '')); ?>
 		<?php echo $form->textFieldRow($model, 'titleId', array('placeholder' => '')); ?>
-		<span style="margin-left:70px; color: #003BB3; font-weight: bold;">Phone Numbers</span><hr style="margin:-8px 0 0 165px;"/>
+		<span style="margin-left:10px; color: #003BB3; font-weight: bold;">Phone Numbers</span><hr style="margin:-8px 0 0 110px;"/>
 		<br />
 		<?php echo $form->textFieldRow($model, 'phone1', array('placeholder' => '')); ?>
 		<?php echo $form->textFieldRow($model, 'phone2', array('placeholder' => '')); ?>
 		
-		<span style="margin-left:70px; color: #003BB3; font-weight: bold;">Address</span><hr style="margin:-8px 0 0 165px;"/>
+		<span style="margin-left:10px; color: #003BB3; font-weight: bold;">Address</span><hr style="margin:-8px 0 0 70px;">
 		<br />
 		<?php echo $form->textFieldRow($model, 'locationId', array('placeholder' => '')); ?>
 		<?php echo $form->textFieldRow($model, 'address', array('placeholder' => '')); ?>
@@ -33,7 +33,7 @@
 		<?php echo $form->textAreaRow($model, 'descr', array('class' => 'company-address')); ?>
 	</div>
 	
-	<div style="width: 350px; margin: 0; padding:0; float: left; margin-left: -90px;">
+	<div style="width: 350px; margin: 0; padding:0; float: left; margin-left: -10px;">
 		<?php echo $form->textFieldRow($model, 'email1', array('placeholder' => '')); ?>
 		
 	</div>
@@ -41,3 +41,7 @@
     $this->endWidget();
     unset($form);
 ?>
+<style>
+	.form-horizontal .control-label { width:100px;}
+	.form-horizontal .controls {margin-left: 120px;}
+</style>

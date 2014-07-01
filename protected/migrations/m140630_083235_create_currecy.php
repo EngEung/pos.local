cd758d01581faf444725bcc9be81b8bfa0c6c9d6
EngEung <engeung@gmail.com>
2014-07-01 12:40:22 +0700

    pos.local

--- protected/migrations/m140630_083235_create_currecy.php
+++ protected/migrations/m140630_083235_create_currecy.php
@@ -2,24 +2,36 @@
 
 class m140630_083235_create_currecy extends CDbMigration
 {
-	public function up()
-	{
-	}
+    public function safeUp()
+    {
+        $this->createTable('currency',
+                array(
+                        'id' =>'pk',
+                        'name' => 'varchar(60) not null',
+                        'exchange_rate' => 'varchar(60)',
+                        'address1' => 'varchar(255) not null',
+                        'address2' => 'varchar(255) not null',
+                        'city' => 'varchar(50) not null',
+                        'state_province_id' => 'int',
+                        'postal_code' => 'varchar(255) not null',
+                        'country_id' => 'int',
+                        'account_number' => 'varchar(255) not null',
+                        'contact' => 'varchar(60) not null',
+                        'phone' => 'varchar(15) not null',
+                        'email' => 'varchar(60) not null',
+                        'fax' => 'varchar(15) not null',
+                        'website' => 'varchar(60) not null',
+                        'created_at' => 'timestamp not null default now()',
+                        'modified_at' => 'datetime',
+                        'created_by' => 'varchar(30)',
+                        'modified_by' => 'varchar(30)',
+                        'active' => 'boolean default true',
+                )
+        );
+    }
+	
+    public function safeDown(){
+            $this->dropTable('suppliers');
+    }
 
-	public function down()
-	{
-		echo "m140630_083235_create_currecy does not support migration down.\n";
-		return false;
-	}
-
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
-
-	public function safeDown()
-	{
-	}
-	*/
 }

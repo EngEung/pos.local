7edc1fb3006092ebab8a4fb6059fbb41b8ade702
Eng Eung <engeung@gmail.com>
2014-07-03 23:24:37 +0700

    pos.local

--- protected/modules/setup/controllers/SupplierController.php
+++ protected/modules/setup/controllers/SupplierController.php
@@ -43,32 +43,29 @@ class SupplierController extends Controller{
 		$this->authenticate();
 		$this->layout = 'setup_layout';
 		$model = new CustomerForm();
-		$cp = new CustomerProcess();
-		if(isset($_GET['custId'])){
-			$model = $cp->getCustomerByID($_GET['custId']);
+		$sp = new SupplierProcess();
+		if(isset($_GET['supId'])){
+			$model = $sp->getCustomerByID($_GET['supId']);
 		}
 		
-		if(isset($_POST['CustomerForm'])){
-			$model->attributes = $_POST['CustomerForm'];
-			$validated=$model->validate();
-			if($validated){
-					
-				$model->firstName = $_POST['CustomerForm']['firstName'];
-				$model->lastName = $_POST['CustomerForm']['lastName'];
-				$model->titleId = $_POST['CustomerForm']['titleId'];
-				$model->phone1 = $_POST['CustomerForm']['phone1'];
-				$model->phone2 = $_POST['CustomerForm']['phone2'];
-				$model->fax = $_POST['CustomerForm']['fax'];
-				$model->email1 = $_POST['CustomerForm']['email1'];
-				$model->locationId = $_POST['CustomerForm']['locationId'];
-				$model->address = $_POST['CustomerForm']['address'];
-				$model->note = $_POST['CustomerForm']['note'];
-				$model->customerTypeId = $_POST['CustomerForm']['customerTypeId'];
-				
-				$model->picture = CUploadedFile::getInstance($model, 'picture');
-				$rc = $cp->createCustomer($model);
+		if(isset($_POST['SupplierForm'])){
+			$model->attributes = $_POST['SupplierForm'];
+			if($model->validate()){
+				$model->address2 = $_POST['SupplierForm']['address2'];
+				$model->cityId = $_POST['SupplierForm']['cityId'];
+				$model->stateProvinceId = $_POST['SupplierForm']['stateProvinceId'];
+				$model->postalCode = $_POST['SupplierForm']['postalCode'];
+				$model->countryId = $_POST['SupplierForm']['countryId'];
+				$model->contact = $_POST['SupplierForm']['contact'];
+				$model->fax = $_POST['SupplierForm']['fax'];
+				$model->email = $_POST['SupplierForm']['email'];
+				$model->website = $_POST['SupplierForm']['website'];
+				$flage = $sp->createSupplier($model);
+				if($flage) Yii::app()->user->setFlash('suc_message', 'Your action has been sucessfully.');
+				else Yii::app()->user->setFlash('error_message', 'Your action has not been sucessfully.');
 			}
 		}
+
 		$this->render('update', array('model' => $model));
 	}
 	

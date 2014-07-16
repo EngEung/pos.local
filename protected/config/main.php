3fa868f2f817a87d31cad0976b5fd3c94e7aff9f
EngEung <engeung@gmail.com>
2014-07-16 13:18:57 +0700

    main

--- protected/config/main.php
+++ protected/config/main.php
@@ -61,9 +61,22 @@ return array(
 		// uncomment the following to enable URLs in path-format
 		
 		// URL routes
+		/*'urlManager'=>array(
+				'urlFormat'=>'path',
+				'rules'=>array(
+						'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+						'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+						'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				),
+				'showScriptName'=>false,
+				'caseSensitive'=>false,
+		),*/
+		// URL routes
 		'urlManager'=>array(
 				'urlFormat'=>'path',
 				'rules'=>array(
+						'<module:\w+>/<controller:\w+>/<action:\w+>'=>'<module>/<controller>/<action>',
+						'<module:\w+>/<controller:\w+>/<action:\w+>/<id:\d+>'=>'<module>/<controller>/<action>',
 						'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 						'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 						'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
